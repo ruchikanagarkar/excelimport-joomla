05502fba031222ccd7fbeb434e60f367d39de5bd
Ruchika Nagarkar <ruchikanagarkar@outlook.com>
2017-02-08 14:01:44 +0530

    unecessary columns removed

--- models/fileupload.php
+++ models/fileupload.php
@@ -31,7 +31,7 @@ class ComponentnameModelFileupload extends JModelAdmin
 	*/
 	public function getValuesSection1($rowData, $data){
 
-		$values = ' "'.$data['section'][0].'" , "'.str_replace("'","",$rowData[0][0]).'" ,"'.str_replace("'","",$rowData[0][6]).'" , "'.$data['year'].'" ,"'.str_replace("'","",$rowData[0][7]).'", "'.$rowData[0][8].'" , "'.$rowData[0][9].'" ,"'.$rowData[0][10].'" , "'.$rowData[0][11].'" , "'.$rowData[0][12].'" ,"'.$rowData[0][13].'" , "'.$rowData[0][14].'" , "'.$rowData[0][15].'" ,"'.$rowData[0][16].'" , "'.$rowData[0][17].'" , "'.$rowData[0][18].'" ,"'.$rowData[0][19].'" , "'.$rowData[0][20].'" , "'.$rowData[0][21].'" ,"'.$rowData[0][22].'" , "'.$rowData[0][23].'" , "'.$rowData[0][24].'" ,"'.$rowData[0][25].'" , "'.$rowData[0][26].'" , "'.$rowData[0][27].'" ,"'.$rowData[0][28].'" , "'.$rowData[0][29].'" , "'.$rowData[0][30].'" ,"'.$rowData[0][31].'" , "'.$rowData[0][32].'" , "'.$rowData[0][33].'" ,"'.$rowData[0][34].'" , "'.$rowData[0][35].'" , "'.$rowData[0][36].'" ,"'.$rowData[0][37].'" , "'.$rowData[0][38].'" , "'.$rowData[0][39].'" ,"'.$rowData[0][40].'" , "'.$rowData[0][41].'" , "'.$rowData[0][42].'" ,"'.$rowData[0][43].'" , "'.$rowData[0][44].'" , "'.$rowData[0][45].'" ';
+		$values = ' "'.$data['section'][0].'" , "'.str_replace("'","",$rowData[0][0]).'" ,"'.str_replace("'","",$rowData[0][6]).'" , "'.$data['year'].'" ,"'.str_replace("'","",$rowData[0][7]).'", "'.$rowData[0][8].'" , "'.$rowData[0][9].'" ,"'.$rowData[0][10].'" , "'.$rowData[0][11].'" , "'.$rowData[0][12].'" ';
 
 	 	return $values;
 	}
@@ -42,7 +42,7 @@ class ComponentnameModelFileupload extends JModelAdmin
 	 * @param   array  $data  section/year values
 	*/
 	public function getValuesSection2($rowData, $data){
-		$values = ' "'.$data['section'][0].'" , "'.str_replace("'","",$rowData[0][0]).'" ,"'.str_replace("'","",$rowData[0][6]).'" , "'.$data['year'].'" ,"'.str_replace("'","",$rowData[0][7]).'", "'.$rowData[0][8].'" , "'.$rowData[0][9].'" ,"'.$rowData[0][10].'" , "'.$rowData[0][11].'" , "'.$rowData[0][12].'" ,"'.$rowData[0][13].'" , "'.$rowData[0][14].'" , "'.$rowData[0][15].'" ,"'.$rowData[0][16].'" , "'.$rowData[0][17].'" , "'.$rowData[0][18].'" ,"'.$rowData[0][19].'" , "'.$rowData[0][20].'" , "'.$rowData[0][21].'" ,"'.$rowData[0][22].'" , "'.$rowData[0][23].'" , "'.$rowData[0][24].'" ,"'.$rowData[0][25].'" , "'.$rowData[0][26].'" , "'.$rowData[0][27].'" ,"'.$rowData[0][28].'" , "'.$rowData[0][29].'" , "'.$rowData[0][30].'" ,"'.$rowData[0][31].'" , "'.$rowData[0][32].'" , "'.$rowData[0][33].'" ,"'.$rowData[0][34].'" , "'.$rowData[0][35].'" , "'.$rowData[0][36].'" ,"'.$rowData[0][37].'" , "'.$rowData[0][38].'" , "'.$rowData[0][39].'" ,"'.$rowData[0][40].'" , "'.$rowData[0][41].'" , "'.$rowData[0][42].'" ,"'.$rowData[0][43].'" , "'.$rowData[0][44].'" , "'.$rowData[0][45].'" ';
+		$values = ' "'.$data['section'][0].'" , "'.str_replace("'","",$rowData[0][0]).'" ,"'.str_replace("'","",$rowData[0][6]).'" , "'.$data['year'].'" ,"'.str_replace("'","",$rowData[0][7]).'", "'.$rowData[0][8].'" , "'.$rowData[0][9].'" ,"'.$rowData[0][10].'" , "'.$rowData[0][11].'" , "'.$rowData[0][12].'" ';
 
 	 	return $values;
 	}
